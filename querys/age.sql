f3f3dee6ad5d047a776cd99cdd61d8c0ec0a35c4
darksider458 <darksider458@gmail.com>
2016-04-09 19:19:57 +0200

    Update age.sql

--- querys/age.sql
+++ querys/age.sql
@@ -1,5 +1,19 @@
-SELECT SUM(case when DATEDIFF(year,dbo.Employes.Date_of_Birth,GETDATE()) between 18 and 24 then 1 else 0 END ) as[18-24],
-	   SUM(case when DATEDIFF(year,dbo.Employes.Date_of_Birth,GETDATE()) between 25 and 36 then 1 else 0 END ) as[25-36],
-	   SUM(case when DATEDIFF(year,dbo.Employes.Date_of_Birth,GETDATE()) between 37 and 90 then 1 else 0 END ) as[37-93]
-  from Employes
 
+SELECT filtred.ranges,sum(filtred.number)  from
+(select 
+(
+case when tab.Age_Difference BETWEEN 18 AND 28 then '18-28'
+	 when tab.Age_Difference BETWEEN 29 AND 38 then '29-38'
+	 when tab.Age_Difference BETWEEN 39 AND 48 then '39-48'
+	 when tab.Age_Difference BETWEEN 49 and 58 then '49-58'
+	 when tab.Age_Difference BETWEEN 59 and 68 then '59-68'
+	 when tab.Age_Difference BETWEEN 69 and 78 then '69-78'
+	 when tab.Age_Difference BETWEEN 79 and 88 then '79-88'
+	 when tab.Age_Difference BETWEEN 89 and 98 then '89-98'
+	 when tab.Age_Difference >=99 then 'over 99'
+	
+end
+) as ranges
+,tab.number from  (select DATEDIFF(year,Customers.Date_of_Birth,GETDATE()) as Age_Difference,COUNT(*) as number from Customers group by DATEDIFF(year,Customers.Date_of_Birth,GETDATE())) as tab
+)as filtred
+group by filtred.ranges
